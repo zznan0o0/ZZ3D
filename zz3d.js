c202199a6918682c26e74b2d1bfd470e1dd875a2
C.y <517701884@qq.com>
2017-06-07 13:43:25 +0800

    3d

--- zz3d.js
+++ zz3d.js
@@ -9,37 +9,37 @@ class canvas3D{
   
   get2DXY(x, y, z){
     let scale = -this.props3D.fallLength / (this.props3D.fallLength + z),
-    X = scale * x + this.props3D.originX,
-    Y = scale * y + this.props3D.originY;
+      X = scale * x + this.props3D.originX,
+      Y = scale * y + this.props3D.originY;
     return {X: X, Y: Y};
   }
 
   rotateX(x, y, z, angle){
     let cos = Math.cos(angle),
-    sin = Math.sin(angle);
+      sin = Math.sin(angle);
 
     let y1 = cos * y - sin * z,
-    z1 = cos * z + sin * y;
+      z1 = cos * z + sin * y;
 
     return {x:x, y:y1, z:z1};
   }
 
   rotateY(x, y, z, angle){
     let cos = Math.cos(angle),
-    sin = Math.sin(angle);
+      sin = Math.sin(angle);
 
     let x1 = cos * x - sin * z,
-    z1 = cos * z + sin * x;
+      z1 = cos * z + sin * x;
 
     return {x:x1, y:y, z:z1};
   }
 
   rotateZ(x, y, z, angle){
     let cos = Math.cos(angle),
-    sin = Math.sin(angle);
+      sin = Math.sin(angle);
 
     let x1 = cos * x - sin * y,
-    y1 = cos * y + sin * x;
+      y1 = cos * y + sin * x;
 
     return {x:x1, y:y1, z:z};
   }
